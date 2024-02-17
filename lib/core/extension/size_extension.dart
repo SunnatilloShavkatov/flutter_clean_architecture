6808d685cfd4c58a38ba5e4886a6a4f12ee4480b
Sunnatillo Shavkatov <sunnatilloshavkatov@gmail.com>
2024-02-17 23:11:29 +0500

    code review

--- lib/core/extension/size_extension.dart
+++ lib/core/extension/size_extension.dart
@@ -15,7 +15,7 @@ extension SizeExtension on BuildContext {
 
   double get aspectRatio => height / width;
 
-  double get devicePixelRatio => MediaQuery.of(this).devicePixelRatio;
+  double get devicePixelRatio => MediaQuery.devicePixelRatioOf(this);
 
   EdgeInsets get viewPadding => MediaQuery.viewPaddingOf(this);
 
